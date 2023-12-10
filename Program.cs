95b1e57c2450e659f8ac6eb47225c53fc902388b
Bradley <barkerbg001@gmail.com>
2023-12-10 10:07:04 +0200

    Adding Sagger Documentation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
+using System.Reflection;
 using DocBuilder.Class;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwagger();
+
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
 // Method 1: Accepts a class, returns a byte[]
-app.MapPost("/method1", (ReportDto input) => 
+app.MapPost("/generatePdf", (ReportDto input) => 
 {
     // Implement your logic here to process 'input' and convert to byte[]
     byte[] result = null;
@@ -12,7 +41,7 @@ app.MapPost("/method1", (ReportDto input) =>
 });
 
 // Method 2: Accepts a class, returns a list of byte[]
-app.MapPost("/method2", (ReportDto input) => 
+app.MapPost("/generateDocumentImages", (ReportDto input) => 
 {
     // Implement your logic here to process 'input' and convert to List<byte[]>
     List<byte[]> resultList = new List<byte[]>();
@@ -28,5 +57,7 @@ app.MapPost("/method3", (ReportDto input) =>
 });
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
